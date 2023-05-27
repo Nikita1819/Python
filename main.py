e7e168f2c8e3e154da81d056d0279a7eb58adb6d
Nikita1819 <98734698+Nikita1819@users.noreply.github.com>
2023-05-27 13:22:30 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,53 +1,65 @@
-A = [2,7,4,6,5]
-i = A.index(max(A))
-proverka=True
-from random import randint
-lst = []
-i = int(input("Введите количесво элементов: "))
-n = int(input("Для заполения массива случайными числа 1,для ручного ввода  2: "))
-if n == 1:
-    lst = [randint(1,20) for i in range(i)]
-    print(lst)
-    maxElem = max(lst)
-    indexMax = lst.index(maxElem)
-    index = indexMax+1
-    for index in range(indexMax+1,len(lst)):
-        if lst[index]%2==0:
-            lst[index]="chet elem"
-    index=0
-    while index<len(lst):
-        if lst[index]=="chet elem":
-            lst.pop(index)
-            ind=indexMax
-        index=index+1
-    print(lst)
+import os
+from pathlib import Path
+import csv
 
-else:
-    for x in range(i):
-        string = "Введите " +str(x+1)+" элемент списка: "
-        lst.append(input(string))
-    for x in lst:
-        if not x.isnumeric():
-            proverka=False
-            print("В списке могут быть только числа")
-            break
-if proverka:
-    listic = [int(x) for x in lst]
-    print(listic)
-    t = 0
-    # find max value in list
-    max_value = max(listic)
-
-    # find index of max value in list
-    max_index = listic.index(max_value)
-    print( max_value, max_index)
-    u = max_index +1
-    while u < len(listic):
-        if listic[u]%2==0:
-            del listic[u]
-        else:
-            u +=1
-    print(listic)
+def countFiles():
+    fileName = input("Введите путь к папке: ")
+    file = Path(fileName)
+    files = os.listdir(file)
+    print(f"В данной папке содержатся следующие объекты: {files}\nИх количество: {len(files)}")
 
 
 
+def get_posts(posts_history):
+    print("post number;nickname;post text;number of likes")
+    with open("Post.csv", "r", encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(*row)
+            posts_history[row[0]] = row[1:]
+
+
+def sort_posts_number(posts_history):
+    print("\nОтсортируем по количеству лайков: ")
+    for elem in sorted(posts_history.items(), key = lambda para: int(para[1][2])):
+        print(elem[0], *elem[1])
+
+
+def sort_posts_str(posts_history):
+    print("\nОтсортируем по тексту поста: ")
+    for elem in sorted(posts_history.items(), key = lambda para: para[1][1]):
+        print(elem[0], *elem[1])
+    print("")
+
+
+def output_by_criterion(posts_history):
+    criterion = input("Введите количсество лайков: \n")
+    print(f'\nСтроки, в которых количество лайков больше "{criterion}": ')
+    for elem in posts_history:
+        if posts_history[elem][2] >= criterion:
+            print(elem, *posts_history[elem])
+
+
+def write_to_csv(posts_history):
+    newKey = input("Введите номер поста: ")
+    new_nickname = input("Введите никнейм пользователя который создал пост: ")
+    newtextpost = input("Введите текст поста: ")
+    new_number_of_likes = input("Введите количество лайков поста: ")
+    posts_history[newKey] = [new_nickname, newtextpost, new_number_of_likes]
+    with open('Post.csv', 'w', encoding='utf-8') as f:
+        for elem in posts_history:
+            f.write(elem + ',' + posts_history[elem][0] + ',' + posts_history[elem][1] + ',' + posts_history[elem][2] + '\n')
+
+
+def main():
+    countFiles()
+    posts_history = {}
+    get_posts(posts_history)
+    sort_posts_number(posts_history)
+    sort_posts_str(posts_history)
+    output_by_criterion(posts_history)
+    write_to_csv(posts_history)
+
+
+if __name__ == "__main__":
+    main()
